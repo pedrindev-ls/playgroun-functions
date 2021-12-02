40b96eaffb71c8f2de0553efcb91ace7a4bad2b0
Pedrin <pedro515vieira@gmail.com>
2021-12-02 17:40:18 -0300

    Resolvendo as primeiras

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,15 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(p1, p2) {
+    if (p1 === true && p2 === true){
+      return true
+    } else {
+      return false
+    }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2
 }
 
 // Desafio 3
@@ -14,36 +18,82 @@ function splitSentence() {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(p1) {
+  return   p1[p1.length - 1] + ', ' + p1[0]
 }
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let pontos = (3 * wins) + ties
+  return pontos
 }
 
 // Desafio 6
 function highestCount() {
-  // seu código aqui
+  
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2,) {
+  let dis1 = cat1 - mouse
+  let dis2 = cat2 - mouse
+  if (dis1 === dis2){
+    return 'os gatos trombam e o rato foge'
+  } else if (dis2 > dis1){
+    return 'cat1'
+  } else if (dis1 > dis2) {
+    return 'cat2'
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  for (let i = 0; i < array.length; i++){
+    if (array[i] % 3 === 0 && array[i] % 5 === 0){
+      array.splice(i, 1, 'fizzBuzz')
+    } else if (array[i] % 3 === 0){
+      array.splice(i, 1, 'fizz')
+    } else if (array[i] % 5 === 0){
+      array.splice(i, 1, 'buzz')
+    } else {
+      array.splice(i, 1, 'bug!')
+    }
+  }
+  return array
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  for (let i = 0; i < string.length; i++){
+    if(string[i] === 'a'){
+      string.replace(string[i], 1)
+    } else if (string[i] === 'e'){
+      string.replace(string[i], 2)
+    } else if (string[i] === 'i'){
+      string.replace(string[i], 3)
+    } else if (string[i] === 'o'){
+      string.replace(string[i], 4)
+    } else if (string[i] === 'u'){
+      string.replace(string[i], 5)
+    }
+  }
+  return string 
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  for (let i = 0; i < string.length; i++){
+    if(string[i] === '1'){
+      string.replace(string[i], 'a')
+    } else if (string[i] === '2'){
+      string.replace(string[i], 'e')
+    } else if (string[i] === '3'){
+      string.replace(string[i], 'i')
+    } else if (string[i] === '4'){
+      string.replace(string[i], 'o')
+    } else if (string[i] === '5'){
+      string.replace(string[i], 'u')
+    }
+  } 
+  return string 
 }
 
 module.exports = {
