130e79ded8ae7b9a5fee172ba6e2f4646f676162
Pedrin <pedro515vieira@gmail.com>
2021-12-03 10:45:05 -0300

    Desafio 12 Concluido, 10/13

--- src/challenges2.js
+++ src/challenges2.js
@@ -9,8 +9,29 @@ function generatePhoneNumber() {
 }
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+
+  let difAB = Math.abs(lineA - lineB)
+  let difBC = Math.abs(lineB - lineC)
+  let difAC = Math.abs(lineA - lineC)
+  
+  let sumAB = lineA + lineB
+  let sumBC = lineB + lineC
+  let sumAC = lineA + lineC
+
+  if(lineA < sumBC && lineA > difBC){
+    if (lineB < sumAC && lineB > difAC){
+      if (lineC < sumAB && lineC > difAB){
+        return true
+      } else {
+        return false
+      }
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
 }
 
 // Desafio 13
